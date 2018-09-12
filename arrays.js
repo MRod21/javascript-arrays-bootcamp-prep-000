3340d09240f6116e35d2b4b15c251718e2782159
Mario Rodriguez <mario.rodriguez35@yahoo.com>
2018-09-12 01:18:36 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element) {
@@ -39,3 +40,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 function removeElementFromEndOfArray(array) {
     return array.slice(0, array.length - 1)
 }
+=======
+var chocolateBars = [snickers, hundred grand, kitkat, skittles]
+>>>>>>> 527e06e3208c3a53d1fa2afe30f9b1d9834952a4
